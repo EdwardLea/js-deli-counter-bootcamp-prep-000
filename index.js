1a4c43951bba7d23f18f490d93b8f30ce9aa29aa
leaedward87 <leaedward87@gmail.com>
2018-11-01 13:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var nowServing = function(katzDeliLine){
 var currentLine = function(katzDeliLine){
   var msg;
   if(katzDeliLine.length >= 1){
-  var msg = "The line is currently: ";
+  msg = "The line is currently: ";
   for (var i = 0; i < katzDeliLine.length; i++){
     msg +=  i+1 + ". " + katzDeliLine[i] +", ";
   }
