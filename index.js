c172c5f9429c6401e0b31b5dcc9d4b4b3e90ce59
leaedward87 <leaedward87@gmail.com>
2018-11-01 13:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var currentLine = function(katzDeliLine){
   for (var i = 0; i < katzDeliLine.length; i++){
     queueMsg +=  i+1 + ". " + katzDeliLine[i] +", ";
   }
-  msg = queueMsg.slice(0, queueMsg.length-1);
+  msg = queueMsg.substring(0, queueMsg.length-1);
   } else{
     msg = "The line is currently empty.";
   }
