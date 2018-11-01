623f60f7383d96acd8f8e5a67ddb4b22ae9bed4c
leaedward87 <leaedward87@gmail.com>
2018-11-01 13:25:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var takeANumber = function(katzDeliLine, name){
+  katzDeliLine.push(name);
+  var queueLength = katzDeliLine.length;
+  return "Welcome, " + name +". You are number " + queueLength +" in line.";
+};
+
+var nowServing = function(katzDeliLine){
+  var lineOutput;
+  if (katzDeliLine.length >= 1){
+    lineOutput = "Currently serving " + katzDeliLine[0] +".";
+    katzDeliLine.shift();
+  } else {
+    lineOutput = "There is nobody waiting to be served!";
+  }
+  return lineOutput;
+};
+
+var currentLine = function(katzDeliLine){
+  var msg;
+  for (var i = 0; i< katzDeliLine.length; i++){
+    msg++ 
+  }
+  
+};
